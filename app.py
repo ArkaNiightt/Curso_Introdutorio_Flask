78971584bff37c047117bfadb28b35630b76dfe2
ArkaNiightt <augustojoao60@yahoo.com>
2024-09-13 07:45:34 -0300

    carditem db

--- app.py
+++ app.py
@@ -25,16 +25,21 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=True)
+    cart = db.relationship("CartItem", backref="user", lazy=True)
 
 
 # Produto (id, name, price, description)
 class Product(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+# Carrinho (id, user_id, product_id)
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
 
 # Auth
 @login_manager.user_loader
